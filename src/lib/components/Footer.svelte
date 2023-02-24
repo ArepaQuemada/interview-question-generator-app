883f2b6fb25ae13485c1fc65848f90a4a6b44bfe
Juan <rigualjuan@gmail.com>
2023-02-24 09:41:10 -0300

    fix: position absolute

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,4 @@
-<footer class="bg-primary-dark sticky w-full pt-4 pb-4 pr-1 pl-1 h-36 md:h-28 text-white">
+<footer class="bg-primary-dark absolute w-full  pb-4 pr-1 pl-1 h-36 md:h-28 text-white bottom-0">
 	<slot />
 	<div class="text-center">
 		<p class="text-gray-400 text-xs md:text-sm mt-2">
