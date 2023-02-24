e460c7ebd7bc5076c734dc2248eddbd91cc4f251
Juan <rigualjuan@gmail.com>
2023-02-24 18:06:14 -0300

    feat: fix size for smartphones

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,6 @@
-<footer class="bg-primary-dark absolute w-full  pb-4 pr-1 pl-1 h-36 md:h-28 text-white bottom-0">
+<footer
+	class="bg-primary-dark absolute w-full mb-4 pt-4 pb-4 pr-1 pl-1 h-36 md:h-28 text-white bottom-0"
+>
 	<slot />
 	<div class="text-center">
 		<p class="text-gray-400 text-xs md:text-sm mt-2">
--- src/lib/components/SkeletonLoading.svelte
+++ src/lib/components/SkeletonLoading.svelte
@@ -1,4 +1,4 @@
-<div role="status" class="animate-pulse w-full mt-10">
+<div role="status" class="animate-pulse w-full my-10 pb-4">
 	<div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5" />
 	<div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5" />
 	<div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5" />
